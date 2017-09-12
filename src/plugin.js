fafc4013577708a7add2b4a75d7d6fc1303377a7
Guillermo Zanoletti <gzanoletti@kaizen.com.ni>
2017-09-12 16:35:58 -0600

    implementation

--- src/plugin.js
+++ src/plugin.js
@@ -1,24 +1,34 @@
 import _ from 'lodash';
 
 const plugin = (editor) => {
-  editor.addButton('fourwsoneh', {
-    text: 'fourwsoneh',
-    icon: false,
-    onclick: () => {
-      // Open window
-      editor.windowManager.open({
-        title: 'fourwsoneh plugin',
-        body: [
-          { type: 'textbox', name: 'title' }
-        ],
-        onsubmit(e) {
-          // Insert content when the window form is submitted
-          const kebabbyString = _.kebabCase(e.data.title);
-          editor.insertContent(kebabbyString);
-        }
-      });
-    }
-  });
+	editor.addButton('fourwsoneh', {
+		text: '4Ws-1H',
+		icon: false,
+		onclick: () => {
+			// Open window
+			editor.windowManager.open({
+				width: 600,
+				height: 230,
+				title: '4Ws and 1H',
+				body: [
+					{type: 'textbox', name: 'w1', label: 'When?'},
+					{type: 'textbox', name: 'w2', label: 'Why?'},
+					{type: 'textbox', name: 'w3', label: 'Whom?'},
+					{type: 'textbox', name: 'w4', label: 'What?'},
+					{type: 'textbox', name: 'h', label: 'How?'},
+				],
+				onsubmit(e) {         
+				  editor.insertContent("<ul>");
+				  editor.insertContent("<li><strong>When?: </strong>" + e.data.w1 + "</li>");
+				  editor.insertContent("<li><strong>Why?: </strong>" + e.data.w2 + "</li>");
+				  editor.insertContent("<li><strong>Whom?: </strong>" + e.data.w3 + "</li>");
+				  editor.insertContent("<li><strong>What?: </strong>" + e.data.w4 + "</li>");
+				  editor.insertContent("<li><strong>How?: </strong>" + e.data.h + "</li>");
+				  editor.insertContent("</ul>");
+				}
+			});
+		}
+	});
 };
 
 export default plugin;
